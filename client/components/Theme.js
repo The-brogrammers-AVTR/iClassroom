170d9a86dbfed97f8764999e47b8e35248a598e3
RobertPeng <jenpopeng@gmail.com>
2020-06-06 01:01:23 -0400

    material ui theme and navbar

--- client/components/Theme.js
+++ client/components/Theme.js
@@ -1,6 +1,4 @@
 import {createMuiTheme} from '@material-ui/core/styles'
-import teal from '@material-ui/core/colors/teal'
-import indigo from '@material-ui/core/colors/indigo'
 
 const Theme = createMuiTheme({
   palette: {
