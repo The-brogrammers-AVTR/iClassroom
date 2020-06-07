67f839986d61544cb68bfc5e9937724a754a4347
Tandid Alam <tandid.alam@gmail.com>
2020-06-07 19:32:50 -0400

    delete buttons working and some styling

--- client/components/AnnouncementCard.js
+++ client/components/AnnouncementCard.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {removeAnnouncement} from '../store'
+import {IconButton} from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
 
 class AnnouncementCard extends React.Component {
   constructor() {
@@ -7,14 +11,18 @@ class AnnouncementCard extends React.Component {
   }
 
   render() {
-    const {title, description} = this.props
+    const {title, description, id} = this.props
 
     return (
       <div className="announcement-card">
         <h3>{title}</h3>
         <p>{description}</p>
-        <button>Edit</button>
-        <button>Delete</button>
+        <IconButton>
+          <EditIcon />
+        </IconButton>
+        <IconButton>
+          <DeleteIcon onClick={() => this.props.remove(id)} />
+        </IconButton>
       </div>
     )
   }
@@ -23,4 +31,12 @@ const mapStateToProps = ({user}) => {
   return {user}
 }
 
-export default connect(mapStateToProps)(AnnouncementCard)
+const mapDispatchToProps = dispatch => {
+  return {
+    remove: id => {
+      dispatch(removeAnnouncement(id))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AnnouncementCard)
--- client/components/LessonCard.js
+++ client/components/LessonCard.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {removeLesson} from '../store'
+import {IconButton} from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
+import EditIcon from '@material-ui/icons/Edit'
 
 class LessonCard extends React.Component {
   constructor() {
@@ -7,14 +11,18 @@ class LessonCard extends React.Component {
   }
 
   render() {
-    const {title, description} = this.props
+    const {title, description, id} = this.props
 
     return (
       <div className="announcement-card">
         <h3>{title}</h3>
         <p>{description}</p>
-        <button>Edit</button>
-        <button>Delete</button>
+        <IconButton>
+          <EditIcon />
+        </IconButton>
+        <IconButton>
+          <DeleteIcon onClick={() => this.props.remove(id)} />
+        </IconButton>
       </div>
     )
   }
@@ -22,5 +30,12 @@ class LessonCard extends React.Component {
 const mapStateToProps = ({user}) => {
   return {user}
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    remove: id => {
+      dispatch(removeLesson(id))
+    }
+  }
+}
 
-export default connect(mapStateToProps)(LessonCard)
+export default connect(mapStateToProps, mapDispatchToProps)(LessonCard)
