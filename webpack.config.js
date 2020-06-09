a7e3f226b3a02553c2fee355a499a63e183c1ccb
AleksandrKayner <AleksandrKayner6@gmail.com>
2020-06-09 00:43:34 -0400

    2

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: './public/bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.css']
+    extensions: ['.js', '.jsx']
   },
   devtool: 'source-map',
   watchOptions: {
