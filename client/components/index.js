805883eec5a6d0ac2f6d9071c0032a76bd636f40
RobertPeng <jenpopeng@gmail.com>
2020-06-11 14:15:27 -0400

    fix one thing in compoents index

--- client/components/index.js
+++ client/components/index.js
@@ -4,7 +4,6 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {Login, Signup} from './auth-form'
 export {default as LoginForm} from './Login.js'
 export {default as SignupForm} from './Signup.js'
 export {default as DrawArea} from './DrawArea'
