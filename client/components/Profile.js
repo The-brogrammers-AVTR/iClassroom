fc0fa15d33e1dc4b21b2df32ffeaf2381a912aa6
Tandid Alam <tandid.alam@gmail.com>
2020-06-10 21:22:31 -0400

    update profile converted to hooks

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,126 +1,91 @@
-import React, {Component} from 'react'
+import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
 import {updateProfile} from '../store'
 
-class Profile extends Component {
-  constructor(props) {
-    let firstName = ''
-    let lastName = ''
-    let email = ''
-    let imageURL = ''
-    if (props.user) {
-      if (props.user.firstName) {
-        firstName = props.user.firstName
-      }
-      if (props.user.lastName) {
-        lastName = props.user.lastName
-      }
-      if (props.user.email) {
-        email = props.user.email
-      }
-      if (props.user.imageURL) {
-        imageURL = props.user.imageURL
-      }
-    }
-    super()
-    this.state = {
-      firstName,
-      lastName,
-      email,
-      imageURL,
-      error: ''
-    }
-    this.onSubmit = this.onSubmit.bind(this)
-  }
+const Profile = ({user, update}) => {
+  const [firstName, setFirstName] = useState(
+    user.firstName ? user.firstName : ''
+  )
+  const [lastName, setLastName] = useState(user.lastName ? user.lastName : '')
+  const [email, setEmail] = useState(user.email ? user.email : '')
+  const [imageURL, setImageURL] = useState(user.imageURL ? user.imageURL : '')
+  const [error, setError] = useState('')
 
-  onSubmit(event) {
-    event.preventDefault()
+  const onSubmit = ev => {
+    ev.preventDefault()
     try {
-      this.props.update(
+      update(
         {
-          id: this.props.user.id,
-          firstName: this.state.firstName,
-          lastName: this.state.lastName,
-          email: this.state.email,
-          imageURL: this.state.imageURL
+          id: user.id,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          imageURL: imageURL
         },
-        this.props.history.push
+        user.id,
+        history.push
       )
     } catch (exception) {
-      this.setState({error: exception.response.data.message})
+      setError({error: exception.response.data.message})
     }
   }
-  componentDidUpdate(prevProps) {
-    if (prevProps.user.firstName !== this.props.user.firstName) {
-      this.setState({firstName: this.props.user.firstName})
-    }
-    if (prevProps.user.lastName !== this.props.user.lastName) {
-      this.setState({lastName: this.props.user.lastName})
-    }
-    if (prevProps.user.email !== this.props.user.email) {
-      this.setState({email: this.props.user.email})
-    }
-    if (prevProps.user.imageURL !== this.props.user.imageURL) {
-      this.setState({imageURL: this.props.user.imageURL})
-    }
-  }
+  useEffect(
+    () => {
+      console.log(firstName, lastName, email, imageURL)
+    },
+    [firstName, lastName, email, imageURL]
+  )
 
-  render() {
-    const {onSubmit} = this
-    const {user} = this.props
-    const {firstName, lastName, email, imageURL} = this.state
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h3>User Profile</h3>
-          <img src={imageURL} />
-        </div>
-        <div>
-          <p className="row">
-            First Name:
-            <input
-              value={firstName}
-              onChange={event => this.setState({firstName: event.target.value})}
-            />
-          </p>
-          <p className="row">
-            Last Name:
-            <input
-              value={lastName}
-              onChange={event => this.setState({lastName: event.target.value})}
-            />
-          </p>
-          <p className="row">
-            Email:
-            <input
-              value={email}
-              onChange={event => this.setState({email: event.target.value})}
-            />
-          </p>
-          <p className="row">
-            imageURL:
-            <input
-              value={imageURL}
-              onChange={event => this.setState({imageURL: event.target.value})}
-            />
-          </p>
-          <button
-            type="submit"
-            onClick={onSubmit}
-            disabled={
-              firstName === user.firstName &&
-              lastName === user.lastName &&
-              email === user.email &&
-              imageURL === user.imageURL
-            }
-          >
-            Update Profile
-          </button>
-        </div>
+        <h3>User Profile</h3>
+        <img src={imageURL} />
       </div>
-    )
-  }
+      <div>
+        <p className="row">
+          First Name:
+          <input
+            value={firstName}
+            onChange={event => setFirstName(event.target.value)}
+          />
+        </p>
+        <p className="row">
+          Last Name:
+          <input
+            value={lastName}
+            onChange={event => setLastName(event.target.value)}
+          />
+        </p>
+        <p className="row">
+          Email:
+          <input
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+        </p>
+        <p className="row">
+          imageURL:
+          <input
+            value={imageURL}
+            onChange={event => setImageURL(event.target.value)}
+          />
+        </p>
+        <button
+          type="submit"
+          onClick={onSubmit}
+          disabled={
+            firstName === user.firstName &&
+            lastName === user.lastName &&
+            email === user.email &&
+            imageURL === user.imageURL
+          }
+        >
+          Update Profile
+        </button>
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = ({user}) => {
@@ -131,7 +96,7 @@ const mapStateToProps = ({user}) => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    update: (user, push) => dispatch(updateProfile(user, push))
+    update: (user, id, push) => dispatch(updateProfile(user, id, push))
   }
 }
 
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -23,6 +23,7 @@ const UserCourse = db.define('UserCourse')
 
 User.hasMany(UserCourse)
 UserCourse.belongsTo(User)
+
 User.belongsToMany(Course, {through: UserCourse})
 Course.belongsToMany(User, {through: UserCourse})
 
