0319791d20032d85c4069a3c49a1ebaaf5535174
RobertPeng <jenpopeng@gmail.com>
2020-06-13 19:45:04 -0400

    save some changes in comment

--- client/components/ManageAssignments.js
+++ client/components/ManageAssignments.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import MaterialTable from 'material-table'
 
-const ManageAssignments = ({assignment, remove, course, save}) => {
+const ManageAssignments = ({assignment, remove, course, save, load}) => {
   if (assignment.length === 0) {
     return null
   }
-  console.log(assignment)
-  console.log(save)
+  console.log('assigns', assignment)
+  //console.log(save)
 
   const dataAssign = assignment.map((assign, idx) => ({
     assignmentid: assign.id,
@@ -15,8 +15,10 @@ const ManageAssignments = ({assignment, remove, course, save}) => {
     courseId: assign.courseId,
     category: assign.category,
     description: assign.description,
+    dueDate: assign.dueDate,
     userId: assign.userId
   }))
+  console.log(dataAssign)
 
   const [state, setState] = useState({
     columns: [
@@ -30,6 +32,24 @@ const ManageAssignments = ({assignment, remove, course, save}) => {
     ],
     data: dataAssign
   })
+  // const [columns, setColumns] = useState (
+  //   [
+  //     {title: 'Assignment ID', field: 'assignmentid'},
+  //     {title: 'Assignment#', field: 'assignNum'},
+  //     {title: 'Assignment', field: 'name'},
+  //     {title: 'Course', field: 'courseId'},
+  //     {title: 'Category', field: 'category'},
+  //     {title: 'Description', field: 'description'},
+  //     {title: 'Due Date', field: 'dueDate'},
+  //     {title: 'Teacher', field: 'userId'}
+  //   ]
+  // )
+  // const [data, setData] = useState(
+  //   dataAssign
+  // )
+
+  //useEffect( () => { dataAssign }, [ state.data ] );
+  console.log('state', state)
 
   return (
     <MaterialTable
@@ -37,28 +57,19 @@ const ManageAssignments = ({assignment, remove, course, save}) => {
       style={{width: '80%', margin: 'auto'}}
       columns={state.columns}
       data={state.data}
+      // columns={columns}
+      // data={data}
       editable={{
-        // onRowAdd: newData =>
-        //   new Promise(resolve => {
-        //     setTimeout(() => {
-        //       resolve()
-        //       setState(prevState => {
-        //         const data = [...prevState.data]
-        //         data.push(newData)
-        //         return {...prevState, data}
-        //       })
-        //     }, 600)
-        //   }),
         onRowAdd: newData =>
           new Promise(resolve => {
-            console.log(newData)
+            console.log('newDate', newData)
             save(newData)
             resolve()
-            setState(prevState => {
-              const data = [...prevState.data]
-              data.push(newData)
-              return {...prevState, data}
-            })
+            // setState(prevState => {
+            //   const data = [...prevState.data]
+            //   data.push(newData)
+            //   return {...prevState, data}
+            // })
           }),
 
         onRowUpdate: (newData, oldData) =>
