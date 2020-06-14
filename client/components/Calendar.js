a5d74fe29d8bb659a912d130be6627e067dbdc89
Tandid Alam <tandid.alam@gmail.com>
2020-06-14 12:14:02 -0400

    calendar appointments showing up

--- client/components/Calendar.js
+++ client/components/Calendar.js
@@ -15,26 +15,42 @@ import {
 import {ThemeProvider} from '@material-ui/styles'
 import {makeStyles, Paper} from '@material-ui/core'
 import theme from './Theme'
+const moment = require('moment')
+const today = moment()
 
-const useStyles = makeStyles({
-  table: {
-    margin: theme.spacing(10, 0),
-    padding: theme.spacing(0),
-    width: '95%'
+export const appointments = [
+  {
+    title: 'Vacation',
+    startDate: new Date(2018, 6, 28),
+    endDate: new Date(2018, 6, 29)
+  },
+  {
+    title: 'Vacation',
+    startDate: new Date(2018, 6, 28),
+    endDate: new Date(2018, 6, 29)
   }
-})
+]
+
+// const useStyles = makeStyles({
+//   table: {
+//     margin: theme.spacing(10, 0),
+//     padding: theme.spacing(0),
+//     width: '95%',
+//   },
+// })
 
 const Calendar = () => {
-  const classes = useStyles()
+  // const classes = useStyles()
+  const [data, setData] = useState(appointments)
   //   render() {
   //     const {data} = this.state
 
   return (
     <ThemeProvider theme={theme}>
       <Paper>
-        <Scheduler>
+        <Scheduler data={data}>
           <ViewState
-            defaultCurrentDate="2018-07-27"
+            defaultCurrentDate={today.format('YYYY-MM-DD')}
             defaultCurrentViewName="Month"
           />
           <DayView startDayHour={9} endDayHour={18} />
