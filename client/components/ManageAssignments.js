0e8e944657c5359edc7d0c1233cc53e0178044d4
RobertPeng <jenpopeng@gmail.com>
2020-06-13 23:52:50 -0400

    fix add assignment func

--- client/components/ManageAssignments.js
+++ client/components/ManageAssignments.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import MaterialTable from 'material-table'
 
 const ManageAssignments = ({assignment, remove, course, save, load}) => {
@@ -6,9 +6,8 @@ const ManageAssignments = ({assignment, remove, course, save, load}) => {
     return null
   }
   console.log('assigns', assignment)
-  //console.log(save)
 
-  const dataAssign = assignment.map((assign, idx) => ({
+  const data = assignment.map((assign, idx) => ({
     assignmentid: assign.id,
     assignNum: idx + 1,
     name: assign.name,
@@ -18,82 +17,58 @@ const ManageAssignments = ({assignment, remove, course, save, load}) => {
     dueDate: assign.dueDate,
     userId: assign.userId
   }))
-  console.log(dataAssign)
+  console.log('update', data)
 
-  const [state, setState] = useState({
-    columns: [
-      {title: 'Assignment ID', field: 'assignmentid'},
-      {title: 'Assignment#', field: 'assignNum'},
-      {title: 'Assignment', field: 'name'},
-      {title: 'Course', field: 'courseId'},
-      {title: 'Category', field: 'category'},
-      {title: 'Description', field: 'description'},
-      {title: 'Teacher', field: 'userId'}
-    ],
-    data: dataAssign
-  })
-  // const [columns, setColumns] = useState (
-  //   [
-  //     {title: 'Assignment ID', field: 'assignmentid'},
-  //     {title: 'Assignment#', field: 'assignNum'},
-  //     {title: 'Assignment', field: 'name'},
-  //     {title: 'Course', field: 'courseId'},
-  //     {title: 'Category', field: 'category'},
-  //     {title: 'Description', field: 'description'},
-  //     {title: 'Due Date', field: 'dueDate'},
-  //     {title: 'Teacher', field: 'userId'}
-  //   ]
-  // )
-  // const [data, setData] = useState(
-  //   dataAssign
-  // )
+  const columns = [
+    {title: 'Assignment ID', field: 'assignmentid'},
+    {title: 'Assignment#', field: 'assignNum'},
+    {title: 'Assignment', field: 'name'},
+    {title: 'Course', field: 'courseId'},
+    {title: 'Category', field: 'category'},
+    {title: 'Description', field: 'description'},
+    {title: 'Teacher', field: 'userId'}
+  ]
 
-  //useEffect( () => { dataAssign }, [ state.data ] );
-  console.log('state', state)
+  const handleAdd = async (newData, resolve) => {
+    await save(newData)
+    resolve()
+  }
 
   return (
     <MaterialTable
       title={`Manage Assignments (Course: ${course.name})`}
       style={{width: '80%', margin: 'auto'}}
-      columns={state.columns}
-      data={state.data}
-      // columns={columns}
-      // data={data}
+      columns={columns}
+      data={data}
       editable={{
         onRowAdd: newData =>
           new Promise(resolve => {
             console.log('newDate', newData)
-            save(newData)
-            resolve()
-            // setState(prevState => {
-            //   const data = [...prevState.data]
-            //   data.push(newData)
-            //   return {...prevState, data}
-            // })
+            handleAdd(newData, resolve)
           }),
 
         onRowUpdate: (newData, oldData) =>
           new Promise(resolve => {
             setTimeout(() => {
               resolve()
-              if (oldData) {
-                setState(prevState => {
-                  const data = [...prevState.data]
-                  data[data.indexOf(oldData)] = newData
-                  return {...prevState, data}
-                })
-              }
+              // if (oldData) {
+              //   setState(prevState => {
+              //     const data = [...prevState.data]
+              //     data[data.indexOf(oldData)] = newData
+              //     return {...prevState, data}
+              //   })
+              // }
             }, 600)
           }),
         onRowDelete: oldData =>
           new Promise(resolve => {
             setTimeout(() => {
               resolve()
-              setState(prevState => {
-                const data = [...prevState.data]
-                data.splice(data.indexOf(oldData), 1)
-                return {...prevState, data}
-              })
+              // setState(prevState => {
+              //   const data = [...prevState.data]
+              //   data.splice(data.indexOf(oldData), 1)
+              //   return {...prevState, data}
+              // })
             }, 600)
           })
       }}
