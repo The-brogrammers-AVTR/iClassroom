fa0ab52c1facc6431bbe8bfa715f402ded2c5317
Vinayak Khare <khare.vinayak@gmail.com>
2020-06-19 17:11:20 -0400

    report card functionality done

--- client/components/ReportCard.js
+++ client/components/ReportCard.js
@@ -1,46 +1,9 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import axios from 'axios'
-import {readUserassignment, getCourses, getDetails} from '../store'
 
 class ReportCard extends Component {
-  /* constructor() {
-    super()
-    this.state={
-        assignments:[],
-
-    }
-  }
-  async componentDidMount (){
-    //const userId= this.props.user.id;
-    const {user,courses}=this.props;
-    const assignments=(await axios.get(`/api/userassignments/${user.id}`)).data
-    console.log(assignments)
-    if (assignments.length){
-        const completedAssignments = assignments.filter(ua=>ua.isComplete===true)
-        const assignmentPerSubj= assignments.reduce((acc,item)=>{
-            console.log(item.courseId)
-            
-            acc[item.courseId]=acc[item.courseId] || 0;
-            
-            acc[item.courseId]=acc[item.courseId] + 1;
-            
-            return acc;
-             
-        },{})  
-        console.log('Assignment count per subject',assignmentPerSubj);                        
-        console.log('completed Assignments:',completedAssignments)
-        //const courseDetails=courses.find(course=>course.id===completedAssignments[0].courseId)
-
-        //console.log('Course Details:',courseDetails)
-
-        this.setState({assignments:completedAssignments})
-    }
-
-} */
   render() {
-    // const {assignments}=this.state;
-    const {user, courses, userassignment, assignment} = this.props
+    const {user, courses, userassignment} = this.props
 
     const allAssignments = userassignment.filter(ua => user.id === ua.userId)
 
@@ -50,29 +13,70 @@ class ReportCard extends Component {
     }
     console.log(userassignment, allAssignments, gradedAssignments)
 
-    let courseGrade = {}
+    let courseGrade = [],
+      index = 0
     if (allAssignments.length) {
-      courseGrade = allAssignments.reduce((acc, item) => {
-        if (acc[item.courseId]) {
-          acc[item.courseId] += item.grade * 1
-        } else {
-          acc[item.courseId] = item.grade * 1
-        }
-        return acc
-      }, {})
+      courseGrade = allAssignments.reduce(
+        (acc, item) => {
+          if (acc[index].courseId === item.courseId) {
+            console.log(
+              'if',
+              acc[index].courseId,
+              acc[index].grade,
+              item.courseId
+            )
+            acc[index].assignCount += 1
+            if (item.isComplete) {
+              acc[index].grade += parseInt(item.grade)
+            }
+            console.log(acc)
+          } else {
+            console.log('else', item.courseId)
+            acc.push({
+              courseId: item.courseId,
+              grade: parseInt(item.grade),
+              assignCount: 1
+            })
+            console.log(acc)
+            index++
+          }
+          return acc
+        },
+        [{courseId: 1, grade: 0, assignCount: 0}]
+      )
+      console.log('finally courseGrade', courseGrade)
+      //remove 1st element if its null or 0 inserted by default above
+      if (courseGrade[0].grade === 0 || ourseGrade[0].grade === null) {
+        courseGrade.splice(0, 1)
+      }
+
+      console.log('-Course Grades-after removing null grades-', courseGrade)
+      console.log('Graded Assignments: ', gradedAssignments)
+
+      console.log(' ReportCard :', this.props)
+      return (
+        <div>
+          <h1> Student Report Card</h1>
+          <h2> Course | Grade </h2>
+          <ul>
+            {courseGrade.map((cg, idx) => {
+              console.log(cg)
+              console.log('inside map')
+              const foundCourse = courses.find(
+                course => course.id === cg.courseId
+              )
+              let grade = Math.round(cg.grade / cg.assignCount, 2)
+              console.log(grade)
+              return (
+                <li key={idx}>
+                  {foundCourse.name} &nbsp;&nbsp;&nbsp;{grade}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      )
     }
-    console.log('-Course Grades--', courseGrade)
-
-    console.log('Graded Assignments: ', gradedAssignments)
-
-    console.log(' ReportCard :', this.props)
-
-    return (
-      <div>
-        <h1> Student Report Card</h1>
-        <h2> Course | Grade </h2>
-      </div>
-    )
   }
 }
 
